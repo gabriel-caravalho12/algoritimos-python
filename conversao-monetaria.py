2698f879f32b8ffc814e25c819f8930f1bb6d6ba
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-06-23 14:43:21 -0300

    conversao monetaria completa

--- conversao-monetaria.py
+++ conversao-monetaria.py
@@ -1,9 +1,9 @@
 import json
-from decimal import Decimal
+from decimal import Decimal, getcontext
 import sys
 import requests
 
-value = input("digite o valor a ser convertido:")
+value = input("digite o valor a ser convertido em reais:")
 
 try:
     float(value)
@@ -12,12 +12,21 @@ except:
     print("Por Favor Digite Um Valor válido ...")
     sys.exit(2)
 
-def calc_value(value):
-    data = requests.get('https://economia.awesomeapi.com.br/last/USD')
-    dolar = json.loads(data.text)
+def resquest_data():
+    url = requests.get('https://economia.awesomeapi.com.br/last/USD')
+    dolar = json.loads(url.text)
     valor = dolar["USDBRL"]
-    print(valor["high"])
-    pass
+    data = valor["high"]
+    # print(type(data))
+    return data
 
+def calc_value(value):
+    x = resquest_data()
+    str(value)
+    getcontext().prec = 4
+    conversao = Decimal(x) * Decimal(value)
+    return conversao
+
+# a = calc_value("seu valor em reais é '{0}'".format(value))
 a = calc_value(value)
-print(a)
+print("o valor convertido será:R$ {0}".format(a))
