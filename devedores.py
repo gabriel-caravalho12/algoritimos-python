5c1f71473335b8ea018b4a2d83fe5fda8718a493
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-06-28 17:43:36 -0300

    commit de gabriel

--- devedores.py
+++ devedores.py
@@ -2,20 +2,27 @@ import csv
 arquivo = open('arquivos/todos-os-clientes.csv')
 arquivodev = open('arquivos/devedores.csv')
 
+
 clientes = arquivo.read()
 devedores = arquivodev.read()
 
+# cli_list = list(clientes)
 
 a = set(clientes.split())
 b = set(devedores.split())
 #
 intersecao = a - b
+nov_intersecao = list(intersecao)
 
-escrever = open('arquivos/intersecao.csv', 'w')
-escrita = csv.writer(escrever)
-escrita.writerows(str(intersecao))
-escrever.close()
+nome = nov_intersecao[0]
+nao_devedores = nome.count(nome)
+# # método de escrita em um arquivo csv
+# # escrever = open('arquivos/intersecao.csv', 'w')
+# # escrita = csv.writer(escrever)
+# # escrita.writerow(["nome", "número", "email", "país", "estado", "renda"])
+# # escrita.writerows(str(intersecao))
+# # escrever.close()
 
-conteudo = a - b
+print(nao_devedores)
 
-print(conteudo)
+print(nov_intersecao.count(nov_intersecao))
