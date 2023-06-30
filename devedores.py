aac9b480dd1303921388816a7a1e73b701568151
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-06-30 17:53:10 -0300

    algoritimo de devedores finalizado !

--- devedores.py
+++ devedores.py
@@ -2,7 +2,7 @@ import csv
 
 
 def search_cli():
-    with open("todos-os-clientes.csv") as csv_file:
+    with open("arquivos/todos-os-clientes.csv") as csv_file:
         searchcliname = set()
         searchclicountry = set()
         clientes = csv.reader(csv_file, delimiter=",", quotechar='"')
@@ -20,9 +20,8 @@ def search_cli():
     return searchcliname, searchclicountry
 
 def search_dev():
-    with open("devedores.csv") as csv_file:
+    with open("arquivos/devedores.csv") as csv_file:
         searchdev = set()
-        searchdevcountry = set()
         devedores = csv.reader(csv_file, delimiter=",", quotechar='"')
         devedores = list(devedores)
         lendev = len(devedores)
@@ -30,24 +29,22 @@ def search_dev():
         while lendev and ind <= 500 - 1:
             linha = devedores[ind]
             nome = linha[0]
-            country = linha[3]
             lendev -= 1
             ind += 1
             searchdev.add(nome)
-            searchdevcountry.add(country)
-    return searchdev, searchdevcountry
+    return searchdev
 
 
 nomecli, countrycli = search_cli()
-nomedev, countrydev = search_dev()
+nomedev = search_dev()
 
-
+country = set()
 intersection = set()
+
 intersection = nomecli - nomedev
 no_dev = len(intersection)
-country = len(countrycli - countrydev)
+country = countrycli
 
 print('o total de não devedores é:{0}'.format(no_dev))
 print('seus nomes são: {0}'.format(intersection))
-print('seus respectivos países são:'.format(country))
-# print(nao_devedores)
+print('seus respectivos países são:{0}'.format(country))
