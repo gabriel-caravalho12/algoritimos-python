67464f0fabf96453f87901ae151eecba77786671
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-06-30 17:43:25 -0300

    committing correct file **

--- devedores.py
+++ devedores.py
@@ -1,28 +1,53 @@
 import csv
-arquivo = open('arquivos/todos-os-clientes.csv')
-arquivodev = open('arquivos/devedores.csv')
 
 
-clientes = arquivo.read()
-devedores = arquivodev.read()
+def search_cli():
+    with open("todos-os-clientes.csv") as csv_file:
+        searchcliname = set()
+        searchclicountry = set()
+        clientes = csv.reader(csv_file, delimiter=",", quotechar='"')
+        clientes = list(clientes)
+        lencli = len(clientes)
+        ind = 0
+        while lencli and ind <= 500 - 1:
+            linha = clientes[ind]
+            nome = linha[0]
+            country = linha[3]
+            lencli -= 1
+            ind += 1
+            searchcliname.add(nome)
+            searchclicountry.add(country)
+    return searchcliname, searchclicountry
 
-# cli_list = list(clientes)
+def search_dev():
+    with open("devedores.csv") as csv_file:
+        searchdev = set()
+        searchdevcountry = set()
+        devedores = csv.reader(csv_file, delimiter=",", quotechar='"')
+        devedores = list(devedores)
+        lendev = len(devedores)
+        ind = 0
+        while lendev and ind <= 500 - 1:
+            linha = devedores[ind]
+            nome = linha[0]
+            country = linha[3]
+            lendev -= 1
+            ind += 1
+            searchdev.add(nome)
+            searchdevcountry.add(country)
+    return searchdev, searchdevcountry
 
-a = set(clientes.split())
-b = set(devedores.split())
-#
-intersecao = a - b
-nov_intersecao = list(intersecao)
 
-nome = nov_intersecao[0]
-nao_devedores = nov_intersecao.count(nov_intersecao)
-# # método de escrita em um arquivo csv
-# # escrever = open('arquivos/intersecao.csv', 'w')
-# # escrita = csv.writer(escrever)
-# # escrita.writerow(["nome", "número", "email", "país", "estado", "renda"])
-# # escrita.writerows(str(intersecao))
-# # escrever.close()
+nomecli, countrycli = search_cli()
+nomedev, countrydev = search_dev()
 
-print(nao_devedores)
 
-print(nov_intersecao.count(nov_intersecao))
+intersection = set()
+intersection = nomecli - nomedev
+no_dev = len(intersection)
+country = len(countrycli - countrydev)
+
+print('o total de não devedores é:{0}'.format(no_dev))
+print('seus nomes são: {0}'.format(intersection))
+print('seus respectivos países são:'.format(country))
+# print(nao_devedores)
