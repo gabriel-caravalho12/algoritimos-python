cae732271c82ad505a6f83cc40cd951909486105
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-07-04 17:32:57 -0300

    diagonal princial porem problemas

--- matriz.py
+++ matriz.py
@@ -5,7 +5,24 @@ matriz = [
     [9, 1, 4, 3],
 ]
 
+indx = 0
 x = matriz[0]
-y = x[0]
-for y in x:
+
+# for y in matriz:
+#     while indx < limit:
+#         print(type(y))
+#         indx += 1
+#     print(x.index(range(0, limit)))
+
+print("o tipo de matriz é:{0}".format(type(matriz)))
+print("o tipo de x é:{0}".format(type(x)))
+
+while len(matriz) == len(x):
+    """
+    enquanto o meu x e y forem iguais imprima o y
+    """
+    y = x[indx]
     print(y)
+    indx += 1
+    x = matriz[indx]
+    print(x)
