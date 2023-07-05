61bf4e6cf82c6a29124de54d1a74307605279329
gabriel <gabrielmoraiscarvalho659@gmail.com>
2023-07-05 13:52:16 -0300

    documentação

--- matriz.py
+++ matriz.py
@@ -4,20 +4,25 @@ matriz = [
     [7, 8, 9, 9],
     [9, 1, 4, 3],
 ]
-
-indx = 0
-x = matriz[0]
 """
 variáveis para melhor entender o que está acontecendo :)
 """
+indx = 0
+x = matriz[0]
+limit = len(matriz) -1
+
 print("o tipo de matriz é:{0}".format(type(matriz)))
 print("o tipo de x é:{0}".format(type(x)))
+
+"""
+a diagonal principal em si
+"""
 
 while len(matriz) == len(x):
     """
     enquanto o meu x e y forem iguais imprima o y
     """
     y = x[indx]
-    indx += 1
+    indx += range(1, limit)
     x = matriz[indx]
     print("o elemento de y é {0}".format(y))
